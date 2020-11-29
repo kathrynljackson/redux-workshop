9272e8a5ae09e72a3e0b0eca4ded7e524da89c82
Kathryn Jackson <kathryn.lorraine.j@gmail.com>
2020-11-29 18:56:16 -0500

    Mod4 Prework

---
+++ src/containers/ToDoList.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ToDo from '../components/ToDo';
+import { connect } from 'react-redux';
+
+const ToDoList = ({ todos }) => {
+  const displayTodos = todos.map(todo => {
+    return (
+      <ToDo
+        {...todo}
+        key={todo.id}
+        text={todo.todo}
+      />
+    )
+  })
+  return (
+    <ul>
+      {displayTodos}
+    </ul>
+  )
+
+}
+
+const mapStateToProps = state => ({
+  todos: state.todos
+});
+
+export default connect(mapStateToProps)(ToDoList);
